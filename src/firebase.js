e5d16c92a58caa88d8fb09fad7c03ac9f43b9496
jaokuean <44114492+jaokuean@users.noreply.github.com>
2021-03-15 22:36:37 +0800

    Default + Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDIm2Qkh5suaWll769SwbPLXuvMLE-ts4",
+    authDomain: "pooldb-b6390.firebaseapp.com",
+    projectId: "pooldb-b6390",
+    storageBucket: "pooldb-b6390.appspot.com",
+    messagingSenderId: "190157167499",
+    appId: "1:190157167499:web:5ff619af63db1df751ed8e",
+    measurementId: "G-6CESK7X59P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var database = firebase.firestore();
+export default database;
