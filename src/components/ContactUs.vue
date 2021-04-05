c7dd296c78d9b573817a56033ba2984b191f0077
ZuoXinyue <e0376991@u.nus.edu>
2021-04-05 16:00:33 +0800

    Updated ContactUs & AboutUs

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="container">
     <navbar></navbar>
-    <p>Contact Us</p>
+    <p id="header">Contact Us</p>
+    <br>
+    <div id="content">
+    <p id="starter">
+      Thank you for being with us! If you have any trouble using our platform, please contact us through the following e-mail:
+    </p>
+    <br>
+    <p id="para1">
+      bt3103.g20@gmail.com
+    </p>
+    <br>
+    </div>
   </div>
 </template>
 <script>
@@ -12,3 +23,30 @@ export default {
   },
 };
 </script>
+<style scoped>
+#header {
+  text-align: center;
+  font-size: 35px;
+}
+
+#starter {
+  font-size: 25px;
+  width: 70%;
+  text-align: center;
+  padding-left: 180px;
+  padding-top: 50px;
+}
+
+#para1 {
+  font-size: 45px;
+  width: 70%;
+  text-align: center;
+  padding-left: 180px;
+  color:white;
+}
+
+#content {
+  background-color:#83d0fd;
+  height: 350px;
+}
+</style>
